50e2c58959812ef35b0b1ee4106765d47e7a0d39
Nfor_Esperance <48797463+SmartNfor@users.noreply.github.com>
2019-12-27 14:07:42 +0100

    Add files via upload

--- routes/api/mesure.js
+++ routes/api/mesure.js
@@ -4,6 +4,7 @@ const router = express.Router();
 // Item Model
 const Mesure = require('../../models/Mesure');
 const Capteur = require('../../models/Capteur');
+const Parcelle = require('../../models/Parcelle');
 
 
 function getMesure(capteur,res) {
@@ -13,25 +14,42 @@ function getMesure(capteur,res) {
         }       
     });
 }
-// @route   GET api/capteur
-// @desc    Get All capteurs
+
+// @route   GET api/mesures
+// @desc    Get all mesures
+// @access  Public
+router.get('/', (req, res) => {
+    Mesure.find()
+      .populate('capteur')
+      .populate('parcelle')
+      .sort({ name: 1 })
+      .then(data => res.json(data))
+      .catch(err => console.log(err));
+  });
+ 
+// @route   GET api/mesures/latest
+// @desc    Get latest mesures for all capteurs
 // @access  Public
 router.get('/latest/', (req, res) => {
   Capteur.find()
-    .sort({ name: 1 })
-    .then(data => getMesure(data, res))
-    .catch(err => console.log(err));
+    .then(capteurs => {
+        capteurs.forEach(capteur =>{
+            Mesure.findOne({ capteur: capteur }).sort('-date').exec(function(err, data) {console.log(data);})
+        })
+    })
+    .catch(err => console.log(err))
+    .finally();
 });
 
-// @route   GET api/capteur
-// @desc    Get one capteur
+// @route   GET api/mesure/id_mesure
+// @desc    Get one mesure
 // @access  Public
 router.get('/:id', (req, res) => {
-    Capteur.findById(req.params.id)
+    Mesure.findById(req.params.id)
     .then(data => {
         if(!data) {
             return res.status(404).send({
-                message: "Capteur not found with id " + req.params.id
+                message: "Mesure not found with id " + req.params.id
             });            
         }
         res.json(data)
@@ -39,33 +57,42 @@ router.get('/:id', (req, res) => {
     .catch(err => {
         if(err.kind === 'ObjectId') {
             return res.status(404).send({
-                message: "Capteur not found with id " + req.params.id
+                message: "Mesure not found with id " + req.params.id
             });                
         }
         return res.status(500).send({
-            message: "Error retrieving capteur with id " + req.params.id
+            message: "Error retrieving Mesure with id " + req.params.id
         });
     });
 });
 
-//@route POST api/capteurs
-//@desc Create a capteur
+//@route POST api/id_parcelle/id_capteurs
+//@desc save the mesures of one capteurs for a particular parcelle
 //@access Public
-router.post("/", (req, res) => {
-    const data = new Capteur({
-      name: req.body.name,
-      type_grandeur:req.body.type,
-      description:req.body.desc,
-      effectif:req.body.effectif,
-    });
-    data.save()
-      .then(data => {
-        res.json(data);
-      }).catch(err => {
-          res.status(500).send({
-              message: err.message || "Some error occurred while creating the Capteur."
-          });
-      });
+router.post("/:parcelle_id/:capteur_id", (req, res) => {
+Capteur.findById(req.params.capteur_id)
+            .then(capteur =>{
+                        Parcelle.findById(req.params.parcelle_id)
+                            .then(parcelle => {
+                                const data = new Mesure({
+                                    grandeur: req.body.grandeur,
+                                    valeur:req.body.valeur,
+                                    capteur:capteur,
+                                    parcelle:parcelle,
+                                });
+                                data.save()
+                                    .then(data => {
+                                    res.json(data);
+                                    })
+                                    .catch(err => {
+                                        res.status(500).send({
+                                            message: err.message || "Some error occurred while creating the Mesure."
+                                        });
+                                    });
+                            })
+                            .catch(err => res.status(404).json({ success: false }));
+              })
+            .catch(err => res.status(404).json({ success: false }));
   });
 
 //@route POST api/capteur
@@ -102,7 +129,7 @@ router.post("/update/:id", (req, res) => {
 //@desc Delete Capteur
 //@access Public
 router.delete("/:id", (req, res) => {
-    Capteur.findById(req.params.id)
+    Mesure.findById(req.params.id)
       .then(data =>
         data
           .remove()
---
+++ routes/api/parcelle.js
@@ -0,0 +1,102 @@
+const express = require('express');
+const router = express.Router();
+
+// Item Model
+const Parcelle = require('../../models/Parcelle');
+
+// @route   GET api/parcelle
+// @desc    Get All parcelle
+// @access  Public
+router.get('/', (req, res) => {
+  Parcelle.find()
+    .sort({ date_semis: 1 })
+    .then(data => res.json(data))
+    .catch(err => console.log(err));
+});
+
+// @route   GET api/parcelle
+// @desc    Get one parcelle
+// @access  Public
+router.get('/:id', (req, res) => {
+    Parcelle.findById(req.params.id)
+    .then(data => {
+        if(!data) {
+            return res.status(404).send({
+                message: "Parcelle not found with id " + req.params.id
+            });            
+        }
+        res.json(data)
+    })
+    .catch(err => {
+        if(err.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "Parcelle not found with id " + req.params.id
+            });                
+        }
+        return res.status(500).send({
+            message: "Error retrieving parcelle with id " + req.params.id
+        });
+    });
+});
+
+//@route POST api/parcelle
+//@desc Create an parcelle
+//@access Public
+router.post("/", (req, res) => {
+    const data = new Parcelle({
+      location: req.body.location,
+      nombre_plant:req.body.nombre_plant,
+    });
+    data.save()
+      .then(data => {
+        res.json(data);
+      }).catch(err => {
+          res.status(500).send({
+              message: err.message || "Some error occurred while creating the Parcelle."
+          });
+      });
+  });
+
+//@route POST api/parcelles
+//@desc Create an parcelle
+//@access Public
+router.post("/update/:id", (req, res) => {
+        Parcelle.findByIdAndUpdate(req.params.id, {
+            location: req.body.location,
+             nombre_plant:req.body.nombre_plant,
+        }, {new: true})
+        .then(data => {
+            if(!data) {
+                return res.status(404).send({
+                    message: "Parcelle not found with id " + req.params.id
+                });
+            }
+            res.json(data);
+        })
+        .catch(err => {
+            if(err.kind === 'ObjectId') {
+                return res.status(404).send({
+                    message: "Parcelle not found with id " + req.params.id
+                });                
+            }
+            return res.status(500).send({
+                message: "Error updating parcelle with id " + req.params.id
+            });
+        });
+  });
+
+//@route DELETE api/parcelle:id
+//@desc Delete an parcelle
+//@access Public
+router.delete("/:id", (req, res) => {
+    Parcelle.findById(req.params.id)
+      .then(data =>
+        data
+          .remove()
+          .then(data => res.json(data))
+          .catch(err => res.json({ success: false }))
+      )
+      .catch(err => res.status(404).json({ success: false }));
+  });
+module.exports = router;
+
