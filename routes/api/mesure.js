d1a667dee1fcbc760be00673deb873587b252a1e
Nfor_Esperance <48797463+SmartNfor@users.noreply.github.com>
2019-12-30 10:50:45 +0100

    Add files via upload

--- routes/api/mesure.js
+++ routes/api/mesure.js
@@ -7,14 +7,6 @@ const Capteur = require('../../models/Capteur');
 const Parcelle = require('../../models/Parcelle');
 
 
-function getMesure(capteur,res) {
-    Mesure.findOne({ capteur: capteur},{ sort: { 'date' : -1 }}, function (err, data) {
-        if(data){
-            res.json(data)
-        }       
-    });
-}
-
 // @route   GET api/mesures
 // @desc    Get all mesures
 // @access  Public
@@ -31,14 +23,22 @@ router.get('/', (req, res) => {
 // @desc    Get latest mesures for all capteurs
 // @access  Public
 router.get('/latest/', (req, res) => {
+    let arr = []
   Capteur.find()
     .then(capteurs => {
-        capteurs.forEach(capteur =>{
-            Mesure.findOne({ capteur: capteur }).sort('-date').exec(function(err, data) {console.log(data);})
+        capteurs.forEach((capteur,index) =>{
+            Mesure.findOne({ capteur: capteur }).sort('-date')
+                .then(data => {
+                    arr.push(data)                   
+                })
+                .then(()=>{
+                    if(index == capteurs.length-1){
+                        res.json(arr)
+                    }
+                })
         })
     })
     .catch(err => console.log(err))
-    .finally();
 });
 
 // @route   GET api/mesure/id_mesure
@@ -66,67 +66,59 @@ router.get('/:id', (req, res) => {
     });
 });
 
+// @route   GET api/mesure/all/id_capteur
+// @desc    Get all the mesure of one capteur
+// @access  Public
+router.get('/all/:capteur_id', (req, res) => {
+    Capteur.findById(req.params.capteur_id)
+    .then(capteur => {
+            Mesure.find({ capteur: capteur })
+                .then(data => {
+                    res.json(data)                  
+                })
+                .catch(err =>{
+                    res.status(404).send({
+                        message: err.message || "No Mesure found"
+                    });
+                })
+    })
+    .catch(err => console.log(err))
+});
+
 //@route POST api/id_parcelle/id_capteurs
 //@desc save the mesures of one capteurs for a particular parcelle
 //@access Public
 router.post("/:parcelle_id/:capteur_id", (req, res) => {
 Capteur.findById(req.params.capteur_id)
-            .then(capteur =>{
-                        Parcelle.findById(req.params.parcelle_id)
-                            .then(parcelle => {
-                                const data = new Mesure({
-                                    grandeur: req.body.grandeur,
-                                    valeur:req.body.valeur,
-                                    capteur:capteur,
-                                    parcelle:parcelle,
-                                });
-                                data.save()
-                                    .then(data => {
-                                    res.json(data);
-                                    })
-                                    .catch(err => {
-                                        res.status(500).send({
-                                            message: err.message || "Some error occurred while creating the Mesure."
-                                        });
-                                    });
-                            })
-                            .catch(err => res.status(404).json({ success: false }));
-              })
-            .catch(err => res.status(404).json({ success: false }));
+        .then(capteur =>{
+            Parcelle.findById(req.params.parcelle_id)
+                .then(parcelle => {
+                    const data = new Mesure({
+                        grandeur: req.body.grandeur,
+                        valeur:req.body.valeur,
+                        capteur:capteur,
+                        parcelle:parcelle,
+                    });
+                    data.save()
+                        .then(data => {
+                        res.json(data);
+                        })
+                        .catch(err => {
+                            res.status(500).send({
+                                message: err.message || "Some error occurred while creating the Mesure."
+                            });
+                        });
+                })
+                .catch(err => res.status(404).json({ success: false }));
+            })
+        .catch(err => res.status(404).json({ success: false }));
   });
 
-//@route POST api/capteur
-//@desc Create an capteur
-//@access Public
-router.post("/update/:id", (req, res) => {
-        Capteur.findByIdAndUpdate(req.params.id, {
-            name: req.body.name,
-            type_grandeur:req.body.type,
-            description:req.body.desc,
-            effectif:req.body.effectif,
-        }, {new: true})
-        .then(data => {
-            if(!data) {
-                return res.status(404).send({
-                    message: "Capteur not found with id " + req.params.id
-                });
-            }
-            res.json(data);
-        })
-        .catch(err => {
-            if(err.kind === 'ObjectId') {
-                return res.status(404).send({
-                    message: "Capteur not found with id " + req.params.id
-                });                
-            }
-            return res.status(500).send({
-                message: "Error updating capteur with id " + req.params.id
-            });
-        });
-  });
+// router.post("/update//:parcelle_id/:capteur_id", (req, res) => {
+//   });
 
-//@route DELETE api/capteur:id
-//@desc Delete Capteur
+//@route DELETE api/mesures:id
+//@desc Delete mesures
 //@access Public
 router.delete("/:id", (req, res) => {
     Mesure.findById(req.params.id)
